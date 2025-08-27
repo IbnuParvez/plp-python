55fdb7b5a9843d198702bdb3c08e7748931806e2
IbnuParvez <parvezfatma98@gmail.com>
2025-08-27 10:05:45 +0300

    First week assignment

--- 1/calculator.py
+++ 1/calculator.py
@@ -1,1 +1,27 @@
+x = float(input("What is your first choice? "))
 
+y = float(input("What is your second number "))
+
+operation = input("Enter your preferred operation (+, -, *, /,): ")
+
+if operation == "+":
+    result = x + y 
+    print(f"{x} + {y} = {result}")
+
+elif operation == "-":
+    result = x - y 
+    print(f"{x} - {y} = {result}")
+
+elif operation == "*":
+    result = x * y 
+    print(f"{x} * {y} = {result}")
+
+elif operation == "/":
+    if y != 0:
+      result = x / y
+      print(f"{x} / {y} = {result}")
+    else:
+      print("Division by zero is not your level boss!!!")
+
+else:
+    print("Please choose the appropriate operations as stipulated")
